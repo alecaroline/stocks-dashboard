eed325b2abcb92db8ff21b681c19fd003ccdeb9f
Alessandra Caroline <alessandracarolineavelino@gmail.com>
2023-05-11 21:53:53 -0300

    Primeiros conceitos de JavaScript: Funções onclink e uso de API

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const openModal = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "flex"
+}
+
+const closeModal = (idModal) => {
+    const divModal = document.querySelector(idModal)
+    divModal.style.display = "none"
+}
+
+const handleModalClose = (event) => {
+    if(event.target.className === "modal"){
+        event.target.style.display = "none"
+    }
+}
+
+const handleAddTicker = async (event) => {
+    event.preventDefault()  //Impede que o form seja enviado
+    const ticker = event.target.ticker.value //pega o valor do input ticker
+    try{
+        const response = await fetch(`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=4GK01SAUUNCXJEV0`) //faz a requisição na API
+        const data = await response.json() // transforma a resposta JSON em objeto
+        console.log(data)
+        if(data["Global Quote"]["05. price"]){
+            alert('Deu certo!')
+        }else{
+            alert(`Ticker ${ticker} não encontrado!`)
+        }
+    } catch(error){
+        alert(error)
+    }
+}
+
+const modal = document.querySelector(".modal")
+modal.addEventListener("click", handleModalClose)
